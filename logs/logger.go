ef1089dd90a1400676e828c1c22ac07ea2537fa2
Chris Snowden <cdsnowden@gmail.com>
2021-06-14 07:39:49 +0100

    refactor(logs): unignore log controller again..

---
+++ logs/logger.go
@@ -0,0 +1,24 @@
+package logs
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	WarningLogger *log.Logger
+	InfoLogger    *log.Logger
+	ErrorLogger   *log.Logger
+)
+
+func init() {
+	// If the file doesn't exist, create it or append to the file
+	file, err := os.OpenFile("/logs/files/log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	InfoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	WarningLogger = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
